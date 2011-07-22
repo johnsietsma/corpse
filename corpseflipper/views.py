e466949cd659479140d83654c6c4a0fe85e54a97
John Sietsma <john.sietsma@gmail.com>
2011-07-22 18:17:49 +1000

    - Remove database. - Show random corpses (not corpse parts).

--- corpseflipper/views.py
+++ corpseflipper/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.views.generic.simple import direct_to_template
+from models import Corpse
+
+def random(request):
+	c = Corpse.objects.order_by('?')[0]
+	extra = { 
+		'head': c.head,
+		'torso': c.torso,
+		'legs': c.legs, 
+		'feet': c.feet
+	}
+	return direct_to_template(request, 'corpseflipper/random.html', extra)
--- settings.py
+++ settings.py
@@ -76,9 +76,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'corpse.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    APP_PATH + '/templates'
 )
 
 INSTALLED_APPS = (
