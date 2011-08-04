c14a96763388f895c911dd2ded9563634429536d
John Sietsma <john.sietsma@gmail.com>
2011-08-05 07:33:53 +1000

    - Fix default view of corpse parts with some very ugly code.

--- corpseflipper/views.py
+++ corpseflipper/views.py
@@ -5,37 +5,47 @@ from models import Corpse, CorpseForm
 
 def random_torso(head_torso_link):
 	corpses = Corpse.objects.filter(head_torso_link=head_torso_link)
-	return random_or_default( corpses, Corpse.default_torso )
+	return random_c( corpses )
 
 def random_legs(torso_legs_link):
 	corpses = Corpse.objects.filter(torso_legs_link=torso_legs_link)
-	return random_or_default( corpses, Corpse.default_legs )
+	return random_c( corpses )
 
 def random_feet(legs_feet_link):
 	corpses = Corpse.objects.filter(legs_feet_link=legs_feet_link)
-	return random_or_default( corpses, Corpse.default_feet )
+	return random_c( corpses )
 
 
-def random_or_default( corpses, default):
+def random_c( corpses ):
 	if corpses.count() > 0:
-		c = corpses.order_by('?')[0].url
-	else:
-		c = Corpse.default_head
-	return c
+		return corpses.order_by('?')[0]
 
 
 def random(request):
 	if Corpse.objects.count() > 0:
+		torso = Corpse.default_torso
+		legs = Corpse.default_legs
+		feet = Corpse.default_feet
+		
 		c = Corpse.objects.order_by('?')[0]
-		head = c.head
-		torso = random_torso(head.head_torso_link)
-		legs = random_legs(torso.torso_legs_link)
-		feet = random_torso(legs.legs_feet_link)
+		head = c.head.url
+		c = random_torso(c.head_torso_link)
+		if c: 
+			torso = c.torso.url
+			c = random_legs(c.torso_legs_link)
+
+		if c:
+			legs = c.legs.url
+			c = random_torso(c.legs_feet_link)
+			
+		if c:
+			feet = c.feet.url
+			
 		extra = { 
-			'head': head.url,
-			'torso': torso.url,
-			'legs': legs.url, 
-			'feet': feet.url
+			'head': head,
+			'torso': torso,
+			'legs': legs, 
+			'feet': feet
 		}
 	else:
 		extra = {
